41ee1f7eff7e8e4d1cd3588f9e85a07ae7f96b89
Martin Millnert <martin@millnert.se>
2016-09-03 15:39:38 +0200

    update regex and repofile

--- git_pull_clone.sh
+++ git_pull_clone.sh
@@ -7,7 +7,7 @@
 
 # https://stackoverflow.com/questions/10312521/how-to-fetch-all-git-branches
 for repourl in `grep -vE '^#' repos.txt` ; do
-	reponame=`echo $repourl | sed -e 's/.*\/\(.*\)\.git$/\1/g'`
+	reponame=`echo $repourl | sed -e 's/.*\/\(.*\)$/\1/g'`
 	echo "Repository: $reponame ( $repourl )"
 	if [ ! -d ${reponame} ]; then
 		(git clone $repourl && echo "Clone of $reponame completed OK!" || echo "Clone of $reponame NOT OK!" &)
